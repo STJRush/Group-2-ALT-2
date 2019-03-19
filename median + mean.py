69e35c42873083506ff2e704806e488c304305fd
Alex Brown <43220714+Alexbrown195@users.noreply.github.com>
2019-03-19 10:07:48 +0000

    Add files via upload

---
+++ median + mean.py
@@ -0,0 +1,7 @@
+import statistics
+
+items=[7,2,8,0,1,8,4,9,11,5]
+
+print (items)
+print("the mean of the list is", statistics.mean(items))
+print("the median of the list is", statistics.mode(items))
