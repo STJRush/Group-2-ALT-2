dfb7a4d955a5784bfef88b0d209904f65a858f5d
Stephen Wilde <36001416+Stephen2212@users.noreply.github.com>
2019-04-04 12:17:50 +0100

    graphing code

---
+++ graph plotting.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+          #x-axis
+plt.plot([0, 1, 2, 3, 4, 5, 6, 7], [0, 1, 2, 3, 4, 5, 6, 7])#y-axis
+plt.axis([0, 10.5, -10.5, 10.5])
+plt.ylabel('GDP growth in 2014')
+plt.xlabel('%GDP spent on education')
+plt.show()
